bd44e442bd489849198f561a93c81982c864022f
Alex Conway <alexconway808@gmail.com>
2014-07-10 19:20:56 -1000

    changed the function names to GET and then returned the values instea of calling the variables

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,25 +3,20 @@ $( document ).ready(function() {
 
   $('#submitButton').click(function() {
   
-    createUsername();
-    createMessages();
-    appendNameandChat();
+    getUsername(); getMessages(); appendNameandChat();
     clearstuff();
   });
 
-  var usernametext = "";
-  var chattext = "";
-
-  function createUsername(){
-    usernametext = $('#username').val();
+  function getUsername(){
+    Return $('#username').val();
   };
 
-  function createMessages(){
-    chattext = $('#messages').val();
+  function getMessages(){
+    Return $('#messages').val();
   };
 
   function appendNameandChat(){
-    $('#chatLog').append('<li>' + usernametext + ' ' + ':' + ' ' + chattext +'</li>');
+    $('#chatLog').append('<li>' + usernametext + ' : ' + chattext +'</li>');
   };
 
   function clearstuff(){
